67984fb5efc832766d14c3e677299b2342138d2f
Christofer Jhon <chrisjhon217@gmail.com>
2025-05-11 15:36:09 -0500

    B5 y mejora Hero

--- Frontend/Inicio.js
+++ Frontend/Inicio.js
@@ -2,19 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const listaPeliculas = document.getElementById("listaPeliculas");
   const peliculas = JSON.parse(localStorage.getItem("peliculas")) || [];
 
-  peliculas.forEach(pelicula => {
-    const peliculaDiv = document.createElement("div");
-    peliculaDiv.classList.add("pelicula");
+  peliculas.forEach(p => {
+    const col = document.createElement("div");
+    col.className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-4";
 
-    peliculaDiv.innerHTML = `
-      <img src="${pelicula.poster}" alt="${pelicula.titulo}">
-      <div class="info">
-        <h3>${pelicula.titulo}</h3>
-        <p>${pelicula.genero} | ${pelicula.duracion}</p>
-        <p>Clasificación: ${pelicula.clasificacion}</p>
+    col.innerHTML = `
+    <div class="card h-100 bg-secondary text-white border-0 pelicula">
+      <img src="${p.poster}" class="card-img-top" alt="${p.titulo}" style="height: 300px; object-fit: cover;">
+      <div class="card-body text-center">
+        <h5 class="card-title">${p.titulo}</h5>
+        <p class="card-text">${p.genero} | ${p.duracion}</p>
+        <p class="card-text"><small>Clasificación: ${p.clasificacion}</small></p>
       </div>
-    `;
+    </div>
+  `;
 
-    listaPeliculas.appendChild(peliculaDiv);
+    listaPeliculas.appendChild(col);
   });
 });
+
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
